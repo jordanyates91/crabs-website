b0fb2e1696f0904ee7f5b6692bf265a6a1d86ec4
jordanyates91 <53835761+jordanyates91@users.noreply.github.com>
2025-07-14 14:01:51 -0700

    Add caption to team photo

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,15 @@ function App() {
                     <p className="about-text">
                       The Butte C.R.A.B.S. were founded in 1977 by Tech students and are a member of the Montana Rugby Union which has clubs spread across Montana, Idaho and Washington. The C.R.A.B.S. practice and play their home games on the <a href="https://maps.app.goo.gl/aCk19QobTHEZLdHX6" target="_blank" rel="noopener noreferrer">field</a> located south of the Montana Tech HPER Complex.
                     </p>
-                    <img 
-                      src={teamPhoto} 
-                      alt="Team Photo @ South HPER field. September 8th, 2022" 
-                      className="team-photo"
-                    />
+                    <div className="team-photo-container">
+                      <img 
+                        src={teamPhoto} 
+                        alt="Team Photo @ South HPER field. September 8th, 2022" 
+                        title="Team Photo @ South HPER field. September 8th, 2022"
+                        className="team-photo"
+                      />
+                      <div className="photo-caption">Team Photo @ South HPER field. September 8th, 2022</div>
+                    </div>
                   </section>
                   <div className="mobile-events">
                     <UpcomingEvents />
